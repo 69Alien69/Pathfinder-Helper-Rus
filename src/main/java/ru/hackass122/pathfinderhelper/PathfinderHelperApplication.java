e6ddb9fc6ec076b97145e15bbd21ffb26ed02554
Nikita <worldofstyks@gmail.com>
2025-04-30 01:05:38 +0300

    Первый коммит

---
+++ src/main/java/ru/hackass122/pathfinderhelper/PathfinderHelperApplication.java
@@ -0,0 +1,13 @@
+package ru.hackass122.pathfinderhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathfinderHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hackass122/pathfinderhelper/PathfinderHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hackass122.pathfinderhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
