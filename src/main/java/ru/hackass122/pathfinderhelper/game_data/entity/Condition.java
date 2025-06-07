3ae3c43c5a60cea9df0c7b620e8b18d03eb4b7d4
Nikita <worldofstyks@gmail.com>
2025-06-07 15:41:09 +0300

    Добавлен Effect в Condition

--- src/main/java/ru/hackass122/pathfinderhelper/game_data/entity/Condition.java
+++ src/main/java/ru/hackass122/pathfinderhelper/game_data/entity/Condition.java
@@ -2,25 +2,26 @@ package ru.hackass122.pathfinderhelper.game_data.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import ru.hackass122.pathfinderhelper.game_data.entity.effect.Effect;
 
 @Entity
 public class Condition extends RuleElement {
 
     @Column
-    private String effect; // TODO: подумать над реализацией
+    private Effect effect; // TODO: подумать над реализацией
 
-    public Condition(String effect) {
+    public Condition(Effect effect) {
         this.effect = effect;
     }
 
     protected Condition() {
     }
 
-    public String getEffect() {
+    public Effect getEffect() {
         return effect;
     }
 
-    public void setEffect(String effect) {
+    public void setEffect(Effect effect) {
         this.effect = effect;
     }
 }
