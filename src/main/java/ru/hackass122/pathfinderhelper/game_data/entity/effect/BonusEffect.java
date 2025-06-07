e60db71a277be11977329c2ad7a036f1ffb323eb
Nikita <worldofstyks@gmail.com>
2025-06-07 15:20:37 +0300

    Исправлена ошибка в BonusEffect

--- src/main/java/ru/hackass122/pathfinderhelper/game_data/entity/effect/BonusEffect.java
+++ src/main/java/ru/hackass122/pathfinderhelper/game_data/entity/effect/BonusEffect.java
@@ -1,6 +1,7 @@
 package ru.hackass122.pathfinderhelper.game_data.entity.effect;
 
 import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 import ru.hackass122.pathfinderhelper.character.model.entity.PlayerCharacter;
 import ru.hackass122.pathfinderhelper.common.enums.EffectType;
 
@@ -8,6 +9,7 @@ import ru.hackass122.pathfinderhelper.common.enums.EffectType;
  Описывает эффект применения бонуса к броскам
  */
 
+@Entity
 @DiscriminatorValue("Bonus")
 public class BonusEffect extends Effect {
 
