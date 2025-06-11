5a20791541edfe3057c1c593c77f950321838c41
Nikita <worldofstyks@gmail.com>
2025-06-12 01:28:02 +0300

    Исправлена аннотация над Effect в классе Condition

--- src/main/java/ru/hackass122/pathfinderhelper/game_data/entity/Condition.java
+++ src/main/java/ru/hackass122/pathfinderhelper/game_data/entity/Condition.java
@@ -1,13 +1,13 @@
 package ru.hackass122.pathfinderhelper.game_data.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import ru.hackass122.pathfinderhelper.game_data.entity.effect.Effect;
 
 @Entity
 public class Condition extends RuleElement {
 
-    @Column
+    @OneToOne
     private Effect effect; // TODO: подумать над реализацией
 
     public Condition(Effect effect) {
