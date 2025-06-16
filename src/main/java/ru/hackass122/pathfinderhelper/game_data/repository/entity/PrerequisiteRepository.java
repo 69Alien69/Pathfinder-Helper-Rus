3cae2819eee8a86ea14196d3482085cb7beaaf5b
Nikita <worldofstyks@gmail.com>
2025-06-16 12:30:49 +0300

    Добавлен репозиторий для Prerequisite

--- src/main/java/ru/hackass122/pathfinderhelper/game_data/repository/entity/PrerequisiteRepository.java
+++ src/main/java/ru/hackass122/pathfinderhelper/game_data/repository/entity/PrerequisiteRepository.java
@@ -3,8 +3,10 @@ package ru.hackass122.pathfinderhelper.game_data.repository.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hackass122.pathfinderhelper.game_data.entity.prerequisite.Prerequisite;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface PrerequisiteRepository extends JpaRepository<Prerequisite, Long> {
     Set<Prerequisite> findAllByCodeIn(Set<String> codes);
+    Optional<Prerequisite> findPrerequisiteByCode();
 }
