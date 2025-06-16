4e3f1d2663bcf1face1744fa3d0aa94097c625ae
Nikita <worldofstyks@gmail.com>
2025-06-16 12:35:44 +0300

    Реализованы необходимые сервисы для Prerequisite

--- src/main/java/ru/hackass122/pathfinderhelper/game_data/service/PrerequisiteService.java
+++ src/main/java/ru/hackass122/pathfinderhelper/game_data/service/PrerequisiteService.java
@@ -1,6 +1,8 @@
 package ru.hackass122.pathfinderhelper.game_data.service;
 
+import ru.hackass122.pathfinderhelper.game_data.dto.request.PrerequisiteCreationRequestDto;
 import ru.hackass122.pathfinderhelper.game_data.dto.response.PrerequisiteResponseDto;
+import ru.hackass122.pathfinderhelper.game_data.dto.response.creation.PrerequisiteCreationResponseDto;
 import ru.hackass122.pathfinderhelper.game_data.entity.prerequisite.Prerequisite;
 
 import java.util.Set;
@@ -8,4 +10,7 @@ import java.util.Set;
 public interface PrerequisiteService {
     Set<Prerequisite> getPrerequisitesByCodes(Set<String> codes);
     Set<PrerequisiteResponseDto> getAllPrerequisiteDtos();
+    PrerequisiteResponseDto getPrerequisiteDtoByCode(String code);
+    PrerequisiteResponseDto createPrerequisite(PrerequisiteCreationRequestDto prerequisiteCreationRequestDto);
+    PrerequisiteCreationResponseDto getPrerequisiteCreationOptions();
 }
--- src/main/java/ru/hackass122/pathfinderhelper/game_data/service/impl/PrerequisiteServiceImpl.java
+++ src/main/java/ru/hackass122/pathfinderhelper/game_data/service/impl/PrerequisiteServiceImpl.java
@@ -1,14 +1,22 @@
 package ru.hackass122.pathfinderhelper.game_data.service.impl;
 
 import org.springframework.stereotype.Service;
+import ru.hackass122.pathfinderhelper.common.enums.PrerequisiteType;
+import ru.hackass122.pathfinderhelper.game_data.dto.request.PrerequisiteCreationRequestDto;
 import ru.hackass122.pathfinderhelper.game_data.dto.response.PrerequisiteResponseDto;
+import ru.hackass122.pathfinderhelper.game_data.dto.response.creation.PrerequisiteCreationResponseDto;
 import ru.hackass122.pathfinderhelper.game_data.entity.prerequisite.Prerequisite;
 import ru.hackass122.pathfinderhelper.game_data.mapper.PrerequisiteDtoMapper;
 import ru.hackass122.pathfinderhelper.game_data.repository.entity.PrerequisiteRepository;
 import ru.hackass122.pathfinderhelper.game_data.service.PrerequisiteService;
+import ru.hackass122.pathfinderhelper.game_data.util.PrerequisiteFactory;
+import ru.hackass122.pathfinderhelper.game_data.util.PrerequisiteMetadataProvider;
 
+import java.util.EnumMap;
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Service
@@ -17,7 +25,8 @@ public class PrerequisiteServiceImpl implements PrerequisiteService {
     private final PrerequisiteRepository prerequisiteRepository;
     private final PrerequisiteDtoMapper prerequisiteDtoMapper;
 
-    public PrerequisiteServiceImpl(PrerequisiteRepository prerequisiteRepository, PrerequisiteDtoMapper prerequisiteDtoMapper) {
+    public PrerequisiteServiceImpl(PrerequisiteRepository prerequisiteRepository,
+                                   PrerequisiteDtoMapper prerequisiteDtoMapper) {
         this.prerequisiteRepository = prerequisiteRepository;
         this.prerequisiteDtoMapper = prerequisiteDtoMapper;
     }
@@ -37,5 +46,35 @@ public class PrerequisiteServiceImpl implements PrerequisiteService {
             prerequisiteResponseDtos.add(prerequisiteDtoMapper.entityToResponseDto(prerequisite));
         }
         return prerequisiteResponseDtos;
+    }
+
+    @Override
+    public PrerequisiteResponseDto getPrerequisiteDtoByCode(String code) {
+
+        Prerequisite prerequisite = prerequisiteRepository.findPrerequisiteByCode(code).orElseThrow();
+
+        return prerequisiteDtoMapper.entityToResponseDto(prerequisite);
+    }
+
+    @Override
+    public PrerequisiteResponseDto createPrerequisite(PrerequisiteCreationRequestDto prerequisiteCreationRequestDto) {
+
+        Prerequisite prerequisite = PrerequisiteFactory.createPrerequisiteFrom(prerequisiteCreationRequestDto.name(),
+                prerequisiteCreationRequestDto.description(),
+                prerequisiteCreationRequestDto.legacy(),
+                prerequisiteCreationRequestDto.type(),
+                prerequisiteCreationRequestDto.data());
+
+        return prerequisiteDtoMapper.entityToResponseDto(prerequisite);
+    }
+
+    @Override
+    public PrerequisiteCreationResponseDto getPrerequisiteCreationOptions() {
+        Set<PrerequisiteType> prerequisiteTypes = EnumSet.allOf(PrerequisiteType.class);
+        Map<PrerequisiteType, Map<String, Object>> dataPerType = new EnumMap<>(PrerequisiteType.class);
+        for (PrerequisiteType type : PrerequisiteType.values()) {
+            dataPerType.put(type, PrerequisiteMetadataProvider.getMetadataFor(type));
+        }
+        return new PrerequisiteCreationResponseDto(prerequisiteTypes, dataPerType);
     }
 }
